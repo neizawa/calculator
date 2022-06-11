582e586fc382103c3b1030fa19f497e7827fd4d7
neizawa <meeteori@gmail.com>
2022-06-11 12:57:49 +0500

    Add an operate function

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case "+":
+            add(num1, num2)
+            break;
+        case "-":
+            subtract(num1, num2)
+            break;
+        case "*":
+            multiply(num1, num2)
+            break;
+        case "/":
+            divide(num1, num2)
+            break;
+    }
+}
