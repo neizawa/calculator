792b7159ba771c568430cafa0fe287233b3734c2
neizawa <meeteori@gmail.com>
2022-06-11 15:33:28 +0500

    Add functions to make calculator work

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const display = document.querySelector(".display");
 const numbers = document.querySelectorAll(".numbers button");
+const operators = document.querySelectorAll(".operators button");
+const equals = document.querySelector(".equals");
 
-const displayValue = display.textContent;
+const value = [];
 
 function add(a, b) {
     return a + b;
@@ -20,6 +22,9 @@ function divide(a, b) {
 }
 
 function operate(operator, num1, num2) {
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+
     switch (operator) {
         case "+":
             return add(num1, num2);
@@ -34,6 +39,36 @@ function operate(operator, num1, num2) {
 
 numbers.forEach(number => {
     number.addEventListener('click', () => {
+        if (display.textContent === "0") {
+            display.textContent = "";
+        }
+
+        if (value.length === 1) {
+            value.length = 0
+            display.textContent = "";
+        }
+        
         display.textContent += number.textContent;
     })
 });
+
+operators.forEach(operator => {
+    operator.addEventListener('click', () => {
+        value.push(display.textContent);
+        value.push(operator.textContent)
+        display.textContent = "0";
+    })
+});
+
+equals.addEventListener('click', () => {
+    let result = 0;
+    value.push(display.textContent);
+    display.textContent = "";
+    for (let i = 1; i < value.length; i += 2) {
+        result += operate(value[i], value[(i - 1)], value[(i + 1)]);
+    }
+    
+    value.length = 0;
+    value.push(result);
+    display.textContent = result;
+})
