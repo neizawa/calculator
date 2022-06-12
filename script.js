4aea6390711a457e85712926c505358bd523e068
neizawa <meeteori@gmail.com>
2022-06-12 16:27:50 +0500

    Add dot key support

--- script.js
+++ script.js
@@ -5,24 +5,24 @@ const equals = document.querySelector(".equals");
 const clear = document.querySelector(".clear");
 const del = document.querySelector(".del");
 const percent = document.querySelector(".percent");
-const audio = new Audio('sounds/vineboom.mp3');
+const audio = new Audio("sounds/vineboom.mp3");
 const value = [];
 
-display.textContent = '0'
+display.textContent = "0"
 let isOperatorOn = false;
 let isResultDisplayed = false;
 
-clear.addEventListener('click', clearDisplay)
-del.addEventListener('click', useDeleteButton)
-percent.addEventListener('click', usePercentButton)
-equals.addEventListener('click', evaluate);
+clear.addEventListener("click", clearDisplay)
+del.addEventListener("click", useDeleteButton)
+percent.addEventListener("click", usePercentButton)
+equals.addEventListener("click", evaluate);
 numbers.forEach(number => {
-    number.addEventListener('click', () => addNumber(number.textContent));
+    number.addEventListener("click", () => addNumber(number.textContent));
 });
 operators.forEach(operator => {
-    operator.addEventListener('click', () => useOperator(operator.textContent))
+    operator.addEventListener("click", () => useOperator(operator.textContent))
 });
-window.addEventListener('keydown', useKeyboard)
+window.addEventListener("keydown", useKeyboard)
 
 function add(a, b) {
     return a + b;
@@ -57,24 +57,25 @@ function operate(operator, num1, num2) {
 }
 
 function addNumber(number) {
-    // If there are more than 10 symbols in display, returns in order to not break calculator
     if (display.textContent.length === 10 && !isOperatorOn) {
         alert("You have reached the limit of numbers!")
         return
     }
 
-    // If there is already a decimal, returns 
+    if (isResultDisplayed && number === ".") {
+        isResultDisplayed = false;
+        display.textContent = "0."
+
+    }
+
     if (number === "." && display.textContent.includes(".")) {
         return;
     }
 
-    // If there is only zero or NaN, removes symbol
     if (display.textContent === "0" && !(number === ".")|| display.textContent === "🤨") {
         display.textContent = "";
     }
 
-    // If operator mode is active and result displayed,
-    // clears display
     if (isOperatorOn || isResultDisplayed) {
         isOperatorOn = false;
         isResultDisplayed = false;
@@ -85,15 +86,14 @@ function addNumber(number) {
 }
 
 function useOperator(operator) {
-    // If operator mode is active, returns
     if (isOperatorOn) {
         value.pop();
         value.push(operator)
         return;
     }
 
-    // Solves equation, if there are two numbers
     value.push(display.textContent);
+
     if (value.length === 3) {
         evaluate();
         value.push(display.textContent);
@@ -104,7 +104,6 @@ function useOperator(operator) {
 }
 
 function evaluate() {
-    // If there is only one number, returns
     if (value.length < 2) {
         return;
     }
@@ -120,11 +119,7 @@ function evaluate() {
         function play() {
             audio.play();
         }
-    }
-
-    // Displays result, if it is to long then it
-    // converts to exponential
-    else {
+    } else {
         display.textContent = "";
         display.textContent = Math.round((operate(value[1], value[0], value[2])) * 1000) / 1000;
         if (display.textContent.length > 10) {
@@ -156,7 +151,7 @@ function usePercentButton() {
 }
 
 function useKeyboard(e) {
-    if(e.key >= 0 && e.key <= 9) addNumber(e.key);
+    if(e.key >= 0 && e.key <= 9 || e.key === "." || e.key === ".") addNumber(e.key);
     if(e.key === "*" || e.key === "/" || e.key === "+" || e.key === "-" ) useOperator(e.key);
     if(e.key === "Backspace") useDeleteButton();
     if(e.key === "Enter") evaluate();
